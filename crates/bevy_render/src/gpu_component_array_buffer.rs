3c34cee496f57ec27f7b6a2fabfac0f879188c1a
Patrick Walton <pcwalton@mimiga.net>
2026-07-02 01:33:38 -0700

    Switch `GpuComponentArrayBuffer` to write to `ShaderStorage`.

--- crates/bevy_render/src/gpu_component_array_buffer.rs
+++ crates/bevy_render/src/gpu_component_array_buffer.rs
@@ -1,59 +1,144 @@
-use crate::{
-    render_resource::{GpuArrayBuffer, GpuArrayBufferable},
-    renderer::{RenderDevice, RenderQueue},
-    Render, RenderApp, RenderSystems,
-};
-use bevy_app::{App, Plugin};
+use crate::storage::ShaderBuffer;
+
+use bevy_app::{App, Plugin, PostUpdate};
+use bevy_asset::RenderAssetUsages;
 use bevy_ecs::{
-    prelude::{Component, Entity},
-    schedule::IntoScheduleConfigs,
-    system::{Commands, Query, Res, ResMut},
+    prelude::Entity,
+    query::{QueryFilter, QueryItem, ReadOnlyQueryData},
+    resource::Resource,
+    system::{Commands, Query, ResMut},
 };
+use bevy_mesh::MeshTag;
+use bytemuck::Pod;
 use core::marker::PhantomData;
+use encase::ShaderType;
+use wgpu::{BufferDescriptor, BufferUsages};
 
 /// This plugin prepares the components of the corresponding type for the GPU
-/// by storing them in a [`GpuArrayBuffer`].
-pub struct GpuComponentArrayBufferPlugin<C: Component + GpuArrayBufferable>(PhantomData<C>);
+/// by storing them in a [`RawBufferVec`].
+pub struct GpuComponentArrayBufferPlugin<C>(PhantomData<C>)
+where
+    C: GpuComponentArrayBuffer;
 
-impl<C: Component + GpuArrayBufferable> Plugin for GpuComponentArrayBufferPlugin<C> {
-    fn build(&self, app: &mut App) {
-        if let Some(render_app) = app.get_sub_app_mut(RenderApp) {
-            render_app.add_systems(
-                Render,
-                prepare_gpu_component_array_buffers::<C>.in_set(RenderSystems::PrepareResources),
-            );
-        }
-    }
+pub trait GpuComponentArrayBuffer: Send + Sync + 'static {
+    type QueryData: ReadOnlyQueryData;
+    type QueryFilter: QueryFilter;
+    type Out: Pod + ShaderType + Default;
 
-    fn finish(&self, app: &mut App) {
-        if let Some(render_app) = app.get_sub_app_mut(RenderApp) {
-            render_app.insert_resource(GpuArrayBuffer::<C>::new(
-                &render_app.world().resource::<RenderDevice>().limits(),
-            ));
+    fn extract_component(item: QueryItem<'_, '_, Self::QueryData>) -> Option<Self::Out>;
+
+    fn buffer_descriptor() -> BufferDescriptor<'static> {
+        BufferDescriptor {
+            label: Some("GPU component array"),
+            mapped_at_creation: false,
+            size: 1,
+            usage: BufferUsages::STORAGE | BufferUsages::COPY_DST | BufferUsages::COPY_SRC,
         }
     }
 }
 
-impl<C: Component + GpuArrayBufferable> Default for GpuComponentArrayBufferPlugin<C> {
+#[derive(Resource)]
+pub struct GpuComponentArray<C>
+where
+    C: GpuComponentArrayBuffer,
+{
+    pub buffer: ShaderBuffer,
+    pub tag_to_entity: Vec<Entity>,
+    phantom: PhantomData<C>,
+}
+
+impl<C> Plugin for GpuComponentArrayBufferPlugin<C>
+where
+    C: GpuComponentArrayBuffer,
+{
+    fn build(&self, app: &mut App) {
+        app.add_systems(PostUpdate, update_components::<C>);
+    }
+
+    fn finish(&self, app: &mut App) {
+        app.init_resource::<GpuComponentArray<C>>();
+    }
+}
+
+impl<C> Default for GpuComponentArrayBufferPlugin<C>
+where
+    C: GpuComponentArrayBuffer,
+{
     fn default() -> Self {
         Self(PhantomData::<C>)
     }
 }
 
-fn prepare_gpu_component_array_buffers<C: Component + GpuArrayBufferable>(
+impl<C> Default for GpuComponentArray<C>
+where
+    C: GpuComponentArrayBuffer,
+{
+    fn default() -> Self {
+        GpuComponentArray {
+            buffer: ShaderBuffer {
+                data: None,
+                buffer_description: C::buffer_descriptor(),
+                asset_usage: RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
+                // FIXME: Is this desired?
+                copy_on_resize: true,
+            },
+            tag_to_entity: vec![],
+            phantom: PhantomData,
+        }
+    }
+}
+
+fn update_components<C>(
     mut commands: Commands,
-    render_device: Res<RenderDevice>,
-    render_queue: Res<RenderQueue>,
-    mut gpu_array_buffer: ResMut<GpuArrayBuffer<C>>,
-    components: Query<(Entity, &C)>,
-) {
-    gpu_array_buffer.clear();
+    query: Query<(Entity, Option<&MeshTag>, C::QueryData), C::QueryFilter>,
+    mut component_array: ResMut<GpuComponentArray<C>>,
+) where
+    C: GpuComponentArrayBuffer,
+{
+    for (entity, maybe_tag, item) in &query {
+        match C::extract_component(item) {
+            None => {
+                // TODO: remove
+            }
+            Some(data) => match maybe_tag {
+                None => {
+                    let tag = component_array.len();
+                    component_array.push(entity, data);
+                    commands.entity(entity).insert(MeshTag(tag as u32));
+                }
+                Some(tag) => {
+                    component_array.set(tag.0 as usize, data);
+                }
+            },
+        }
+    }
+}
 
-    let entities = components
-        .iter()
-        .map(|(entity, component)| (entity, gpu_array_buffer.push(component.clone())))
-        .collect::<Vec<_>>();
-    commands.try_insert_batch(entities);
+impl<C> GpuComponentArray<C>
+where
+    C: GpuComponentArrayBuffer,
+{
+    fn len(&self) -> usize {
+        match self.buffer.data {
+            None => 0,
+            Some(ref data) => data.len() / size_of::<C::Out>(),
+        }
+    }
 
-    gpu_array_buffer.write_buffer(&render_device, &render_queue);
+    fn push(&mut self, entity: Entity, data: C::Out) {
+        self.buffer
+            .data
+            .get_or_insert_default()
+            .extend_from_slice(bytemuck::cast_slice(&[data]));
+        self.tag_to_entity.push(entity);
+        debug_assert_eq!(
+            self.buffer.data.as_ref().unwrap().len(),
+            self.tag_to_entity.len()
+        );
+    }
+
+    fn set(&mut self, index: usize, data: C::Out) {
+        bytemuck::cast_slice_mut(self.buffer.data.get_or_insert_default().as_mut_slice())[index] =
+            data;
+    }
 }
